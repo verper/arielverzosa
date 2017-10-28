5e9b0ba778be3b9dbce7ebb21e476dc5ed812ba1
henry-lm <henry@lightmedia.com.au>
2017-10-29 00:36:14 +0800

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,16 +3,16 @@ var app = angular.module("myApp", ['ngRoute']);
 app.config(function($routeProvider, $locationProvider) {
     $routeProvider
     .when("/", {
-        templateUrl : "/blocks/home.html"
+        templateUrl : "blocks/home.html"
     })
     .when("/skills", {
-        templateUrl : "/blocks/skills.html"
+        templateUrl : "blocks/skills.html"
     })
     .when("/portfolio", {
-        templateUrl : "/blocks/portfolio.html"
+        templateUrl : "blocks/portfolio.html"
     })
 	.when("/contact", {
-        templateUrl : "/blocks/contact.html"
+        templateUrl : "blocks/contact.html"
     });
 
     $locationProvider.html5Mode(true);
